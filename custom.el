955065ca3a3a8dd1602bfab3d08774b93eaa4076
Mahmudul Hasan <cs.mahmud@gmail.com>
2025-01-22 20:43:40 -0700

    Added larger .emacs file

---
+++ custom.el
@@ -0,0 +1,18 @@
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(cperl-close-paren-offset -3)
+ '(cperl-continued-statement-offset 3)
+ '(cperl-indent-level 3)
+ '(cperl-indent-parens-as-block t)
+ '(cperl-tab-always-indent t)
+ '(package-selected-packages
+   '(cask-mode markdown-mode inf-clojure clojure-mode rainbow-delimiters lisp-mode cider magit use-package paredit)))
